033a5947678403b88ec8995022c8227b3eebaa28
Agustín <49756731+Agusabb@users.noreply.github.com>
2021-06-19 22:22:04 -0300

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,111 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_hub as hub
+
+# Create a function to load a trained model
+def load_model(model_path):
+  """
+  Loads a saved model from a specified path.
+  """
+  print(f"Loading saved model from:{model_path}")
+  model = tf.keras.models.load_model(model_path,
+                                     custom_objects={"KerasLayer":hub.KerasLayer})
+  return model
+
+
+# Keras
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+model = MobileNetV2(weights='imagenet')
+
+#from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow_hub as hub
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = "C:/Users/agust/Desktop/Deployment-Deep-Learning-Model-master/Deployment-Deep-Learning-Model-master/my_model.h5"
+
+# Load your trained model
+model = keras.models.load_model(MODEL_PATH, custom_objects={"KerasLayer":hub.KerasLayer})
+model.make_predict_function()          # Necessary
+print(f'{model} loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+
+# TESTEOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+model = MobileNetV2(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(224, 224))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(x, data_format=None)
+
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        pred_class = tf.keras.applications.mobilenet_v2.decode_predictions(preds, top=1)   # ImageNet Decode
+        result = str(pred_class[0][0][1])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+app.run(debug=True)
+
